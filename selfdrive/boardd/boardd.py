7bebc6f10f6e7b7e1fe0a8ff7caac0200320daba
fabio71c <fabio@brasvirtual.com>
2024-08-31 13:23:58 -0400

    test

--- selfdrive/boardd/boardd.py
+++ selfdrive/boardd/boardd.py
@@ -1,12 +1,10 @@
 # pylint: skip-file
 
 # Cython, now uses scons to build
-from openpilot.selfdrive.boardd.boardd_api_impl import can_list_to_can_capnp
-assert can_list_to_can_capnp
-
 import time
 from selfdrive.swaglog import cloudlog
 from openpilot.common.params import Params
+from openpilot.selfdrive.boardd.boardd_api_impl import can_list_to_can_capnp
 
 class FakePanda:
     def __init__(self):
@@ -22,24 +20,35 @@ class FakePanda:
     def can_send(self, addr, dat, bus):
         return True
 
-def boardd_loop(fake_panda):
+    def health_check(self):
+        return True
+
+def boardd_loop(fake_panda, sendcan, logcan, pandad_thread):
     cloudlog.info("Starting boardd loop with fake panda")
     params = Params()
-    params.put("PandaSignatures", fake_panda.serial)
 
     while True:
         health = fake_panda.get_health()
         cloudlog.debug(f"Fake panda health: {health}")
-        time.sleep(0.1)
+
+        # Send an empty list of CAN messages
+        can_sends = []
+        sendcan.send(can_list_to_can_capnp(can_sends, msgtype='sendcan'))
+
+        # Receive an empty list of CAN messages
+        can_rcv = fake_panda.can_recv()
+        logcan.send(can_list_to_can_capnp(can_rcv, msgtype='can'))
+
+        time.sleep(0.01)
 
 def panda_connect():
     cloudlog.info("Connecting to fake panda")
     return FakePanda()
 
-def main():
+def main(sendcan, logcan, pandad_thread):
     cloudlog.info("Boardd is using a fake panda")
     fake_panda = panda_connect()
-    boardd_loop(fake_panda)
+    boardd_loop(fake_panda, sendcan, logcan, pandad_thread)
 
 if __name__ == "__main__":
     main()
